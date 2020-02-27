78aaf3468a5fb26c200be83252022f2d5cb2691e
user <yoosa3004@naver.com>
2020-02-27 14:03:41 +0900

    4. 사진 업로드 페이지 만들기 사진을 등록하면 Firebase Storage에 올라간다.

--- app/src/main/java/com/example/hyunndy_instagram/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_instagram/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.hyunndy_instagram
 
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.hyunndy_instagram.navigation.AlarmFragment
 import com.example.hyunndy_instagram.navigation.DetailViewFragment
 import com.example.hyunndy_instagram.navigation.GridFragment
@@ -13,6 +17,7 @@ import com.example.hyunndy_instagram.navigation.UserFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -28,6 +33,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo->{
+                if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED){
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm->{
@@ -50,6 +58,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setContentView(R.layout.activity_main)
 
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.INTERNET), 1)
 
     }
 }
